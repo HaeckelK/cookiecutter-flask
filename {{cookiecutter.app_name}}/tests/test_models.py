ada62926ac67473ef5b5f5ea3330cdbc6ad3033c
HaeckelK <haeckelk.github@gmail.com>
2020-11-03 10:06:21 +0000

    docs: black formatting

--- {{cookiecutter.app_name}}/tests/test_models.py
+++ {{cookiecutter.app_name}}/tests/test_models.py
@@ -67,8 +67,8 @@ class TestUser:
 
     def test_roles_repr(self):
         """Check __repr__ output for Role."""
-        role = Role(name='user')
-        assert role.__repr__() == '<Role(user)>'
+        role = Role(name="user")
+        assert role.__repr__() == "<Role(user)>"
 
     def test_repr(self):
         """Check __repr__ output for User."""
